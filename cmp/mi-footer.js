bb7bcd4fc0e9a5f606f06ce2a147fb927c025aca
JimenezMario <81141032+JimenezMario@users.noreply.github.com>
2021-03-22 09:57:09 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement{
   connectedCallback(){
     this.innerHTML = /* html */
-      `Copyright &copy; 2Jimenez Escamilla mario Emmanuel 2021.`;
+      `Copyright &copy; 2021 Jimenez Mario.`;
   }
 }
 customElements.define("mi-footer", MiFooter);
